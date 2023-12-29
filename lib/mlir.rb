2cc5c240235544b1bb5ecdcb175d267c6b23cf63
johnlinvc <johnlinvc@gmail.com>
2023-12-29 08:03:13 +0800

    Initial commit

---
+++ lib/mlir.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mlir/version"
+
+module Mlir
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mlir/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Mlir
+  VERSION = "0.1.0"
+end
